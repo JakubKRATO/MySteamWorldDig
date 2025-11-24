18467d4b74416d5e66c4d299e1d3087be4abaece
JakubKRATO <kubko.kratochvil@gmail.com>
2025-11-24 22:01:47 +0100

    save

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ db = connection.cursor()
 @app.route("/start-run")
 def startRun():
     worldId = uuid.uuid4()
-    db.execute("INSERT INTO games (user_id,world_id,completed) VALUES (%s, 0);", (session["user_id"],worldId))
+    db.execute("INSERT INTO games (user_id, world_id, completed) VALUES (%s, %s, 0);", (session["user_id"],worldId))
     connection.commit()
 
     return {"uuid": worldId}
